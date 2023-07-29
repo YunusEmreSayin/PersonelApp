2aac24f323531222e21b262be0189a3ae690542d
Yunus Emre Sayın <ynssyn3436@gmail.com>
2023-07-29 11:56:16 +0300

    PersonelApı'nin eklenmesi

---
+++ PersonelAPI/Model/Personel.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonelAPI.Model
+{
+    public class Personel
+    {
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
