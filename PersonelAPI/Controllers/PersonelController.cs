55094ecf05a55e1afa78a11db1b336d115cb5a7a
Yunus Emre Sayın <87127328+YunusEmreSayin@users.noreply.github.com>
2023-08-01 15:31:05 +0300

    Add files via upload

---
+++ PersonelAPI/Controllers/PersonelController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonelAPI.Model;
+using System.Data.SqlClient;
+
+namespace PersonelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonelController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        public PersonelController(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+        [HttpPost]
+        [Route("insertpersonel")]
+        public void insertPersonel(Personel personel)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("PersonelConnection").ToString());
+           
+            String cmdstring = "INSERT INTO personel(Ad,Soyad) VALUES(@Ad,@Soyad)";
+            SqlCommand cmd = new SqlCommand(cmdstring, con);
+            createPersonelParams(cmd, personel);
+            
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        private void createPersonelParams(SqlCommand cmd,Personel personel)
+        {
+            cmd.Parameters.AddWithValue("@Ad", personel.Ad);
+            cmd.Parameters.AddWithValue("@Soyad", personel.Soyad);
+        }
+        
+
+       
+    }
+
+}
---
+++ PersonelAPI/Controllers/getPersonelController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PersonelAPI.Model;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+namespace PersonelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class getPersonelController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        public getPersonelController(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+        [HttpGet]
+        [Route("selectpersonel")]
+        public ActionResult<Personel> GetPersonel()
+        {
+            List<Personel> personelList = new List<Personel>();
+
+            try
+            {
+                SqlConnection con = new SqlConnection(_configuration.GetConnectionString("PersonelConnection").ToString());
+                String cmdstring = "SELECT TOP 1 * FROM personel ORDER BY ID DESC";
+                SqlCommand cmd = new SqlCommand(cmdstring, con);
+                con.Open();
+                var dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Personel personel = new Personel();
+
+                    personel.Ad = dr["Ad"].ToString();
+                    personel.Soyad = dr["Soyad"].ToString();
+                    personel.ID = (int)dr["ID"];
+                    personelList.Add(personel);
+
+                }
+                dr.Close();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+              //  return ex.Message;
+            }
+            string message = System.Text.Json.JsonSerializer.Serialize(personelList);
+            return Content(message);
+           
+
+        }
+
+        
+
+       
+    }
+
+}
